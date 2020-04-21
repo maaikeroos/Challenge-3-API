db3fc808c8d159826c2d7645fde9683830c385e4
maaikeroos <61278422+maaikeroos@users.noreply.github.com>
2020-04-21 21:23:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ map2.on('load', function () {
       })
       .then(function(response) {
         //Het weer met het bijbehorende icoon
-        plotImageOnMap(response.weather[0].icon, response.main.temp, city)
+        plotImageOnMap(response.weather[0].icon, Math.round(response.main.temp) + "\xB0C", city)
       })
       .catch(function (error) {
         console.log('ERROR:', error);
